9a2a2869b39db36067a05ec75b9175c1b3f2eeee
unknown <ikorpan@tktk.ee>
2021-09-03 23:41:35 +0300

    create hello world

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Who R U?");
+            string UserName;
+            UserName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + UserName);
+
+            Console.Read();
+
+
+        }
+    }
+}
